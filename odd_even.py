7f5cf334df0ee36f2bc76431d5e8b207ebbaba5e
ABarnes97 <abarne87@students.kennesaw.edu>
2016-10-29 00:24:59 -0400

    Odd or Even
    Determines whether user input number is an odd or even number using Python

---
+++ odd_even.py
@@ -0,0 +1,9 @@
+
+def odd_even():
+    user_input = input(["Enter a number :"])
+    if(user_input % 2 == 0):
+        print(((str(user_input)) + " is an even number"))
+    else:
+        print((str(user_input)) + " is an odd number")
+
+odd_even()
